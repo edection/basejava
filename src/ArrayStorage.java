aa9553550faa6ee55daa758b203892576483af1a
edection <edection@gmail.com>
2021-02-09 16:26:16 +0300

    Basejava Lesson 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,57 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int storageSize;
 
     void clear() {
+        for (int i = 0; i < storageSize; i++) {
+            storage[i] = null;
+        }
+        storageSize = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[storageSize] = resume;
+        storageSize++;
     }
 
     Resume get(String uuid) {
+        int i;
+        for (i = 0; i < storageSize; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int i;
+        for (i = 0; i < storageSize; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+                break;
+            }
+        }
+        for (i = i; i < storageSize - 1; i++) {
+            storage[i] = storage[i + 1];
+        }
+        storageSize--;
+        storage[storageSize] = null;
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
+        /**
+         * @return array, contains only Resumes in storage (without null)
+         */
+        Resume[] getAll() {
+            return Arrays.copyOf(storage, storageSize);
+        }
 
-    int size() {
-        return 0;
-    }
+        int size() {
+            return storageSize;
+        }
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -6,7 +6,7 @@ public class MainTestArrayStorage {
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
-        r1.uuid = "uuid1";
+        r1.uuid = "uuid1"; //
         Resume r2 = new Resume();
         r2.uuid = "uuid2";
         Resume r3 = new Resume();
